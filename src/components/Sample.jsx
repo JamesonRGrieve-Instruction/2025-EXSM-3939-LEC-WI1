b0a35c91faaad80efbe808f1e803263fd101ac13
James Grieve <jamesonrgrieve@gmail.com>
2025-02-24 16:07:51 -0700

    Modernize;

--- src/components/Sample.jsx
+++ src/components/Sample.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import styles from './Sample.module.css';
 
 const Sample = ({ heading, bodyText, buttonText }) => {
   return (
-    <div className={styles.sample}>
-      <h1 className={styles.heading}>{heading}</h1>
-      <p className={styles.bodyText}>{bodyText}</p>
-      <button className={styles.button}>{buttonText}</button>
+    <div className='container'>
+      <h1>{heading}</h1>
+      <p className='text-center'>{bodyText}</p>
+      <button>{buttonText}</button>
     </div>
   );
 };
