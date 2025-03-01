12bf926beb62aabc314165fee17956e24975e819
James Grieve <jamesonrgrieve@gmail.com>
2025-02-28 19:55:06 -0700

    Basic story with enclosing container (additional args besides props);

---
+++ src/components/stateless/HelloWorld.stories.tsx
@@ -0,0 +1,23 @@
+import HelloWorldComponent, { HelloWorldProps } from './HelloWorld';
+
+// Default export with title
+export default {
+  title: 'Stateless/HelloWorld',
+  component: HelloWorldComponent,
+};
+
+// Named export for each story
+export const HelloWorld = (args: { props: HelloWorldProps; containerStyles?: string }) => (
+  <div className={args.containerStyles}>
+    <HelloWorldComponent {...args.props} />
+  </div>
+);
+
+// Default values for props
+HelloWorld.args = {
+  containerStyles: 'container',
+  props: {
+    firstName: 'Joe',
+    lastName: 'Smith',
+  },
+};
