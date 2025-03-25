24fa6d42b2c2975b8a482eade8aafaf54cafdd44
James Grieve <jamesonrgrieve@gmail.com>
2025-03-24 19:23:08 -0600

    Migrate the functionality into context for reuse;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,15 @@
 'use client';
-import { JSX, useCallback } from 'react';
-import useSampleContext, { SampleContext } from '../components/context/SampleContext';
+import { JSX } from 'react';
+import useSampleContext from '../components/context/SampleContext';
 import CartList from '../components/stateful/CartList';
 
 export default function Home(): JSX.Element {
   const sampleContext = useSampleContext();
-  const handleAddItem = useCallback(
-    (itemCode: string) => {
-      sampleContext.mutate((old: SampleContext) => {
-        const newValue = [
-          ...old.filter((item) => item.itemID !== itemCode),
-          {
-            itemID: itemCode,
-            quantity: (old.find((item) => item.itemID === itemCode) ?? { quantity: 0 }).quantity + 1,
-          },
-        ];
-        console.log('Updating Context', newValue);
-        return newValue;
-      });
-    },
-    [sampleContext],
-  );
   return (
     <>
       <main>
-        <button onClick={() => handleAddItem('PEN101')}>Add Pen to Cart</button>
-        <button onClick={() => handleAddItem('PENCIL101')}>Add Pencil to Cart</button>
+        <button onClick={() => sampleContext.addItem('PEN101')}>Add Pen to Cart</button>
+        <button onClick={() => sampleContext.addItem('PENCIL101')}>Add Pencil to Cart</button>
       </main>
       <aside>
         <CartList />
--- src/components/context/SampleContext.tsx
+++ src/components/context/SampleContext.tsx
@@ -1,21 +1,38 @@
 'use client';
-import { createContext, useContext, useEffect, useState } from 'react';
+import { createContext, useCallback, useContext, useEffect, useState } from 'react';
 export type SampleContextItem = { itemID: string; quantity: number };
 export type SampleContext = SampleContextItem[];
-export type SampleContextHook = { state: SampleContext; mutate: any };
+export type SampleContextHook = { state: SampleContext; mutate: any; addItem: any };
 const initialState: SampleContext = [];
 // Provider
 export function SampleContextProvider({ children }: { children: React.ReactNode }) {
   const [contextState, setContextState] = useState<SampleContext>(initialState);
+  const handleAddItem = useCallback(
+    (itemCode: string) => {
+      setContextState((old: SampleContext) => {
+        const newValue = [
+          ...old.filter((item) => item.itemID !== itemCode),
+          {
+            itemID: itemCode,
+            quantity: (old.find((item) => item.itemID === itemCode) ?? { quantity: 0 }).quantity + 1,
+          },
+        ];
+        console.log('Updating Context', newValue);
+        return newValue;
+      });
+    },
+    [contextState],
+  );
   useEffect(() => {
     console.log(contextState);
   }, [contextState]);
-  return <SampleContext.Provider value={{ state: contextState, mutate: setContextState }}>{children}</SampleContext.Provider>;
+  return <SampleContext.Provider value={{ state: contextState, mutate: setContextState, addItem: handleAddItem }}>{children}</SampleContext.Provider>;
 }
 // Context
 export const SampleContext = createContext<SampleContextHook>({
   state: initialState,
   mutate: () => {},
+  addItem: () => {},
 });
 // Usage Hook
 export default function useSampleContext() {
